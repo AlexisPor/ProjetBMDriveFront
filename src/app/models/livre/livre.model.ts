640e60f74450a2645f8e4651436a47af9dfd6dd1
Unknown <jennifer.devred.afpa@gmail.com>
2021-06-17 17:33:31 +0200

    MAJ des attributs des models

--- src/app/models/livre/livre.model.ts
+++ src/app/models/livre/livre.model.ts
@@ -3,6 +3,6 @@ import { Article } from "../article/article.model";
 export class Livre{
 
   public libId: number;
-  public TArticle: Article = new Article();
+  public bmdArticle: Article = new Article();
   public livNbPages: number;
 }
