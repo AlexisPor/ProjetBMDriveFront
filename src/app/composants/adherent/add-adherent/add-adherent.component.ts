28a2e77635cc031b130340bbfc7713e8b8089ed7
Alexis <alexis19971712@gmail.com>
2021-06-18 11:51:08 +0200

    Ajout d'adherent ok

--- src/app/composants/adherent/add-adherent/add-adherent.component.ts
+++ src/app/composants/adherent/add-adherent/add-adherent.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Adherent } from 'src/app/models/adherent/adherent.model';
+import { Adresse } from 'src/app/models/adresse/adresse.model';
+import { Identite } from 'src/app/models/identite/identite.model';
+import { AdherentService } from 'src/app/services/adherent/adherent.service';
 
 @Component({
   selector: 'app-add-adherent',
@@ -10,12 +14,50 @@ export class AddAdherentComponent implements OnInit {
 
   myFormAdh: FormGroup;
 
-  constructor() { }
+  constructor(private adhService: AdherentService,
+              private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initFormAdh();
   }
+
+  private initFormAdh() {
+    this.myFormAdh = this.fb.group({
+      adhNomUtilisateur: [""],
+      adhMotDePasse: [""],
+      bmdIdentite: this.fb.group({
+        ideNom: [""],
+        idePrenom: [""],
+        ideAge: [""],
+        ideTelephone: [""],
+        ideMail: [""],
+        ideSexe: [""],
+          bmdAdresse: this.fb.group({
+            adrNumRue: [""],
+            adrNomRue: [""],
+            adrCplAdr: [""],
+            adrCp: [""],
+            adrVille: [""]
+
+          }),
+        }),
+    });
+  };
 
   public onSubmit() {
+    const dataAdh = this.myFormAdh.value;
 
-  }
+    let adherent: Adherent = new Adherent();
+
+    adherent.bmdIdentite.bmdAdresse = dataAdh.bmdAdresse;
+    adherent.bmdIdentite = dataAdh.bmdIdentite;
+    adherent.adhNomUtilisateur = dataAdh.adhNomUtilisateur;
+    adherent.adhMotDePasse = dataAdh.adhMotDePasse;
+
+    this.adhService.addAdherent(adherent).subscribe(
+      (response) => {
+        console.log(`Adherent ajouté ${{response}}`);
+
+      });
+  };
 }
