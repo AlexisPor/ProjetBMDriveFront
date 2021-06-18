931e4848098aa7040626d783592cdec0d1cffb72
risbo59300 <ybgabamano@gmail.com>
2021-06-18 11:23:09 +0200

    Ajout des support média ok

--- src/app/composants/suppMedia/add-suppmedia/add-suppmedia.component.ts
+++ src/app/composants/suppMedia/add-suppmedia/add-suppmedia.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SuppMedia } from 'src/app/models/suppMedia/suppmedia.model';
+import { SuppmediaService } from 'src/app/services/suppMedia/suppmedia.service';
 
 @Component({
   selector: 'app-add-suppmedia',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddSuppmediaComponent implements OnInit {
 
-  constructor() { }
+  suppmed : SuppMedia = new SuppMedia();
+
+  ajouter = false;
+
+  constructor(
+    private supmediaService : SuppmediaService,
+    private router : Router,
+    private route : ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  saveSuppMeda() : void {
+    const data = {
+      type : this.suppmed.supType,
+      titre : this.suppmed.bmdArticle.artTitre,
+      auteur : this.suppmed.bmdArticle.artAuteur,
+      datedemprunt : this.suppmed.bmdArticle.artDateEmprunt,
+      dateretour : this.suppmed.bmdArticle.artDateRetour,
+      cote : this.suppmed.bmdArticle.artCote
+    };
+
+    console.log("data = "+data);
+    this.supmediaService.addSuppMedia(this.suppmed)
+    .subscribe(data => {
+      this.ajouter = true;
+      this.goToSuppMediaList();
+    },
+    error => { console.log(error);
+    });
+  }
+
+  goToSuppMediaList() {
+    this.router.navigate(['/list-suppmedia']);
+  }
+
+  onSubmit() {
+    this.saveSuppMeda();
   }
 
 }
