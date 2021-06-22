ab60f31e380ede01e022b670211191fc3df8175b
Unknown <jennifer.devred.afpa@gmail.com>
2021-06-22 16:52:47 +0200

    Maj méthode add agentBM

--- src/app/composants/agentBM/add-agentbm/add-agentbm.component.ts
+++ src/app/composants/agentBM/add-agentbm/add-agentbm.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Agentbm } from 'src/app/models/agentBM/agentbm.model';
+import { AgentbmService } from 'src/app/services/agentBM/agentbm.service';
 
 @Component({
   selector: 'app-add-agentbm',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddAgentbmComponent implements OnInit {
 
-  constructor() { }
+  myFormAgentBM : FormGroup;
+
+
+
+  constructor(private fb: FormBuilder,
+              private agentBMService:AgentbmService
+    ) { }
 
   ngOnInit(): void {
+    this.initFormAgentBM();
+
   }
+
+  onSubmit(){
+
+    const dataAgentBM=this.myFormAgentBM.value;
+          let newAgentBM : Agentbm=new Agentbm();
+          newAgentBM.abmNomUtilisateur=dataAgentBM.abmNomUtilisateur;
+          newAgentBM.abmMotDePasse=dataAgentBM.abmMotDePasse;
+         
+          this.agentBMService.addAgentBM(newAgentBM).subscribe(
+            (data)=>{
+              console.log(newAgentBM)
+            }
+            
+          )
+
+  }
+
+  initFormAgentBM(){
+    this.myFormAgentBM=this.fb.group({
+      abmNomUtilisateur:[""],
+      abmMotDePasse:[""],
+    })
+  }
+
 
 }
