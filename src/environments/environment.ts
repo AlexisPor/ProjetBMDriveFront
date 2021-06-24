b3b857e2b745ae1ad99e32a31927912d6c50e0b6
Alexis <alexis19971712@gmail.com>
2021-06-24 10:24:18 +0200

    Modif des url dans environnement et gestion des conflits

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,29 +5,16 @@
 export const environment = {
   production: false,
   urlAdh: 'http://localhost:8888/adherent/adhMap/',
-  urlAdr: 'http://localhost:8888/adresse/adrMap',
-<<<<<<< HEAD
-  urlAg: 'http://localhost:8888/agentBM/agMap',
-  urlArt: 'http://localhost:8888/ar/artMap/',
-=======
+  urlAdr: 'http://localhost:8888/adresse/adrMap/',
   urlAg: 'http://localhost:8888/agentBM/agMap/',
-  urlArt: 'http://localhost:8888/ar/artMap',
->>>>>>> BMD-142-suprimer-agentbm-front
-  urlCas: 'http://localhost:8888/casier/casMap',
-  urlCat: 'http://localhost:8888/categorie/catMap',
-  urlIden: 'http://localhost:8888/identite/ideMap',
-  urlLiv: 'http://localhost:8888/livre/livMap',
-  urlPan: 'http://localhost:8888/panier/panMap',
-  urlRes: 'http://localhost:8888/reservation/resMap',
+  urlArt: 'http://localhost:8888/ar/artMap/',
+  urlCas: 'http://localhost:8888/casier/casMap/',
+  urlCat: 'http://localhost:8888/categorie/catMap/',
+  urlIden: 'http://localhost:8888/identite/ideMap/',
+  urlLiv: 'http://localhost:8888/livre/livMap/',
+  urlPan: 'http://localhost:8888/panier/panMap/',
+  urlRes: 'http://localhost:8888/reservation/resMap/',
   urlSm: 'http://localhost:8888/sm/smMap/',
-  urlVis: 'http://localhost:8888/visiteur/visMap'
+  urlVis: 'http://localhost:8888/visiteur/visMap/'
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
